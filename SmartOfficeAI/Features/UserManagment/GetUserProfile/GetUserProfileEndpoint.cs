545fbdd66bcd9a415eda0c5b621270d9462f72c7
AbdElRahmanMDev <abdelrahman.m.elsayedd@gmail.com>
2025-10-07 11:17:39 +0300

    UpdateGetUserProfileById

--- SmartOfficeAI/Features/UserManagment/GetUserProfile/GetUserProfileEndpoint.cs
+++ SmartOfficeAI/Features/UserManagment/GetUserProfile/GetUserProfileEndpoint.cs
@@ -1,29 +1,30 @@
-﻿using Mapster;
-using Microsoft.AspNetCore.Mvc;
-using SmartOfficeAI.Common.Views;
+﻿using Microsoft.AspNetCore.Mvc;
 using SmartOfficeAI.Features.Common;
 using SmartOfficeAI.Features.UserManagment.GetUserProfile.Query;
 using SmartOfficeAI.Features.UserManagment.GetUserProfile.VMs;
 
 namespace SmartOfficeAI.Features.UserManagment.GetUserProfile
 {
-    public class GetUserProfileEndpoint : EndpointBase<UserProfileRequestViewModel, UserProfileResponseViewModel>
+    public class GetUserProfileEndpoint : EndpointBase<UserProfileRequestViewModel, IActionResult>
     {
         public GetUserProfileEndpoint(EndpointBaseParameters<UserProfileRequestViewModel> dependencyCollection) : base(dependencyCollection)
         {
         }
 
         [HttpGet("api/user/profile")]
-        public async Task<EndPointResponse<UserProfileResponseViewModel>> GetDisabilityById([FromQuery] UserProfileRequestViewModel request)
+        public async Task<IActionResult> GetDisabilityById([FromQuery] UserProfileRequestViewModel request)
         {
             var validationResult = await ValidateRequestAsync(request);
-            if (!validationResult.IsSuccess) return validationResult;
+            if (!validationResult.IsSuccess)
+                return BadRequest(validationResult);
 
-            var userProfile = await _mediator.Send(new GetUserProfileByIdQuery(request.Id));
+            var result = await _mediator.Send(new GetUserProfileByIdQuery(request.Id));
 
-            return userProfile.IsSuccess ?
-                    EndPointResponse<UserProfileResponseViewModel>.Success(userProfile.Data.Adapt<UserProfileResponseViewModel>(), userProfile.Message) :
-                    EndPointResponse<UserProfileResponseViewModel>.Failure(userProfile.ErrorCode, userProfile.Message);
+            if (result.IsSuccess)
+                return Ok(result.Data);
+
+            var statusCode = result.ErrorCode.StatusCode ?? StatusCodes.Status400BadRequest;
+            return StatusCode(statusCode, result.ErrorCode);
         }
 
     }
