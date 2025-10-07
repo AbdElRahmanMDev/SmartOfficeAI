8b03ab4b02bf7397ccfc928ad1d75981fff20782
AbdElRahmanMDev <abdelrahman.m.elsayedd@gmail.com>
2025-10-07 11:06:43 +0300

    AddNEWEndpoint

--- SmartOfficeAI/Features/UserManagment/ReviewUser/ReviewUserRequest.cs
+++ SmartOfficeAI/Features/UserManagment/ReviewUser/ReviewUserRequest.cs
@@ -1,5 +1,23 @@
-﻿namespace SmartOfficeAI.Features.UserManagment.ReviewUser
+﻿using FluentValidation;
+
+namespace SmartOfficeAI.Features.UserManagment.ReviewUser
 {
     public record ReviewUserRequest(Guid UserId, string Action, Guid? NewRole = null);
+
+    public class ReviewUserRequestValidator : AbstractValidator<ReviewUserRequest>
+    {
+        public ReviewUserRequestValidator()
+        {
+            RuleFor(x => x.UserId)
+                .NotEmpty()
+                .WithMessage("UserId is required.");
+
+            RuleFor(x => x.Action)
+                .NotEmpty()
+                .WithMessage("Action is required.")
+                .Must(action => action == "Approve" || action == "Reject")
+                .WithMessage("Action must be either 'Approve' or 'Reject'.");
+        }
+    }
 
 }
